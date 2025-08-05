5c50cc3ee5f9f42aa5ab1a0a2b72e5d15ddc3945
Pavan1997project <pavanpkumar097@gmail.com>
2025-08-05 20:55:23 +0530

    Update test.ts

--- src/test.ts
+++ src/test.ts
@@ -1,18 +1,1 @@
-// This file is required by Karma test runner to initialize the Angular testing environment
 
-import 'zone.js';
-import 'zone.js/testing';
-
-import { getTestBed } from '@angular/core/testing';
-import {
-  BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting,
-} from '@angular/platform-browser-dynamic/testing';
-
-// First, initialize the Angular testing environment.
-getTestBed().initTestEnvironment(
-  BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting(),
-);
-
-// Then you can import your tests here or Karma will automatically find them.
