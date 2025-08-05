db5cf0e4d2d4b6f2d9ce93caac23651296b026a8
Pavan1997project <pavanpkumar097@gmail.com>
2025-08-05 20:51:27 +0530

    Update karma.cong.js

---
+++ karma.cong.js
@@ -0,0 +1,40 @@
+// karma.conf.js
+
+module.exports = function (config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage'),
+      require('@angular-devkit/build-angular/plugins/karma')
+    ],
+    client: {
+      jasmine: {},
+      clearContext: false // leave Jasmine Spec Runner output visible in browser
+    },
+    coverageReporter: {
+      dir: require('path').join(__dirname, './coverage/login-app'),
+      subdir: '.',
+      reporters: [
+        { type: 'html' },
+        { type: 'text-summary' }
+      ]
+    },
+    reporters: ['progress', 'kjhtml'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ['ChromeHeadless'],
+    singleRun: false,
+    restartOnFileChange: true,
+    // Ensures polyfills.ts and main entrypoints are loaded in the right order
+    files: [
+      { pattern: './src/polyfills.ts', included: true, watched: true },  // ensures Zone.js and polyfills loaded
+      { pattern: './src/test.ts', included: true, watched: true }
+    ]
+  });
+};
