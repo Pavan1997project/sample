610ed6e56bab22402482058c00844a891e3b404e
Pavan1997project <pavanpkumar097@gmail.com>
2025-08-06 10:39:53 +0530

    Update login.spec.ts

--- src/app/login/login.spec.ts
+++ src/app/login/login.spec.ts
@@ -1,16 +1,14 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { Login } from './login';
 
 describe('Login', () => {
-  let component: Login;
   let fixture: ComponentFixture<Login>;
+  let component: Login;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Login]
-    })
-    .compileComponents();
+      imports: [Login] // ✅ for standalone components
+    }).compileComponents();
 
     fixture = TestBed.createComponent(Login);
     component = fixture.componentInstance;
